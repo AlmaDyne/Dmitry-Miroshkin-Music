cebba6f7add6d9439b65c6f0901fe53b5003ae08
AlmaDyne <123851079+AlmaDyne@users.noreply.github.com>
2024-06-18 11:24:07 +0300

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -1,24 +1,32 @@
 'use strict';
 
-document.addEventListener('pointerdown', function() {
-    let range = new Range();
+const navigate = document.querySelector('.navigate');
 
-    document.onselectionchange = function() {
-        let {anchorNode, anchorOffset, focusNode, focusOffset} = document.getSelection();
+navigate.addEventListener('click', function(event) {
+    const targetA = event.target.closest('a');
+    if (!targetA) return;
 
-        range.setStart(anchorNode, anchorOffset);
-        range.setEnd(focusNode, focusOffset);
+    const anchors = [...navigate.children].filter(a => a !== targetA && !a.classList.contains('current-page'));
 
-        if (range.collapsed) {
-            range.setStart(focusNode, focusOffset);
-            range.setEnd(anchorNode, anchorOffset);
-        }
-    };
+    shuffle(anchors);
 
-    document.addEventListener('pointerup', function deleteSelection() {
-        range.deleteContents();
-        
-        document.onselectionchange = () => false;
-        document.removeEventListener('pointerup', deleteSelection);
-    });
+    const newA = anchors[0];
+    targetA.href = newA.getAttribute('href');
+
+    alert('Ты хотел перейти на страницу "' + targetA.textContent + '"?\n' +
+        'Ха! Однако, ты перейдёшь на страницу "' + newA.textContent + '".');
 });
+
+navigate.oncontextmenu = function(event) {
+    const targetA = event.target.closest('a');
+    if (targetA) event.preventDefault();
+};
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
